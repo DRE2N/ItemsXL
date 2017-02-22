e981516edf3357029cf072370deece243e671365
Daniel Saukel <sa.da@unitybox.de>
2017-02-22 23:23:04 +0100

    Update dependencies

--- src/main/java/io/github/dre2n/itemsxl/command/MainCommand.java
+++ src/main/java/io/github/dre2n/itemsxl/command/MainCommand.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015-2016 Daniel Saukel
+ * Copyright (C) 2015-2017 Daniel Saukel
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -55,7 +55,7 @@ public class MainCommand extends BRCommand {
         MessageUtil.sendCenteredMessage(sender, IMessages.COMMAND_MAIN_LOADED.getMessage(String.valueOf(itemList.size())));
         MessageUtil.sendCenteredMessage(sender, IMessages.COMMAND_MAIN_COMPATIBILITY.getMessage(CompatibilityHandler.getInstance().getInternals().toString()));
         MessageUtil.sendCenteredMessage(sender, IMessages.COMMAND_MAIN_HELP.getMessage());
-        MessageUtil.sendCenteredMessage(sender, "&7\u00a92015-2016 Daniel Saukel; licensed under GPLv3.");
+        MessageUtil.sendCenteredMessage(sender, "&7\u00a92015-2017 Daniel Saukel; licensed under GPLv3.");
     }
 
 }
--- src/main/java/io/github/dre2n/itemsxl/item/ItemBox.java
+++ src/main/java/io/github/dre2n/itemsxl/item/ItemBox.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015-2016 Daniel Saukel
+ * Copyright (C) 2015-2017 Daniel Saukel
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
